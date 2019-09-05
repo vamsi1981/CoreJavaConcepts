a713753f5faa3d83624eace5935989fda201babb
vamsi1981 <vamsi.futurama@gmail.com>
2019-09-05 19:04:04 -0400

    VariablesTest

---
+++ VariablesTest.java
@@ -0,0 +1,20 @@
+package org;
+
+
+ class VariablesTest {
+
+
+public static void main(String args[]) {
+	
+ int x=12;
+ int y=13;
+ boolean isEmployed=true;
+ boolean isMarried=false;
+ x=y+23;
+ if(x<26 && isEmployed && !isMarried)
+ {
+	 System.out.println("Person is an eligible bachelor");
+ }
+
+	
+}}
